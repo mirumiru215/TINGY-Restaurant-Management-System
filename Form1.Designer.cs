ce4feea946be640ce89d1b1b21c6c4de5d0b12a4
Miru <ngockhanhmy2003@gmail.com>
2023-04-20 01:54:34 +0700

    Add icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -204,6 +204,7 @@
             this.Controls.Add(this.sidebarPanel);
             this.Controls.Add(this.UCPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
